38fa3ae222484225b56f85d7cdffe6f18ead80b5
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-02-18 12:08:13 -0800

    commit tree 9b72d2beaef550148fc57a5a698f43111f2f9eae

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, setListings] = useState([])
+
+
+  useEffect( () => {
+    fetch("http://localhost:6001/listings")
+      .then((response) => response.json())
+      .then((data) => {setListings(data)})
+    }
+    , [])
+
+
+    function deleteListing(id){
+      const updatedList = listings.filter(listing => {
+        return listing.id !== id
+      })
+      setListings(updatedList)
+
+    }
+
+
+  function handleSubmit(input){
+    const updatedList = listings.filter(listing => {
+      
+      return listing.description.toLowerCase().includes(input.toLowerCase())
+      
+    })
+    setListings(updatedList)
+  }
+
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSubmit ={handleSubmit}/>
+      <ListingsContainer listings = {listings} onDelete = {deleteListing}/>
     </div>
   );
 }
